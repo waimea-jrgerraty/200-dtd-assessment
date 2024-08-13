ca80bc1da68669b2a5ef51575a184e625881b1dc
waimea-jrgerraty <169000524+waimea-jrgerraty@users.noreply.github.com>
2024-08-13 23:42:57 +1200

    finish subtask backend
    adds subtask deletion, completion toggling, and updating the completion field on the parent task

--- website/ServerFunctions.php
+++ website/ServerFunctions.php
@@ -3,6 +3,42 @@ require_once 'components/utils.php';
 $db = connectToDB();
 $type = $_POST ? $_POST['type'] : $_REQUEST['type'];
 
+function updateTaskCompletion($id) {
+    $get = "SELECT `completed` FROM `subtask` WHERE `linked` = ?";
+    $db = connectToDB();
+
+    try {
+        $stmt = $db->prepare($get);
+        $stmt->execute([$id]);
+        $subtasks = $stmt->fetchAll();
+    }
+    catch (PDOException $e) {
+        consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
+        die('There was an error updating task completion');
+    }
+
+    $total = 0;
+    $count = 0;
+
+    foreach($subtasks as $subtask) {
+        $count++;
+        $total += $subtask['completed'];
+    }
+
+    $fraction = $total / max($count, 1) * 100;
+
+    $upd = "UPDATE `tasks` SET `completion` = ? WHERE `id` = ?";
+
+    try {
+        $stmt = $db->prepare($upd);
+        $stmt->execute([$fraction, $id]);
+    }
+    catch (PDOException $e) {
+        consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
+        die('There was an error updating task completion');
+    }
+}
+
 switch ($type) {
     case "sCategory": // Add new supercategory
         // find the max priority from the database
@@ -183,8 +219,19 @@ switch ($type) {
         }
         break;
     case "subtaskDelete":
+        $getVal = "SELECT `linked` FROM `subtask` WHERE id = ?";
         $rem = "DELETE FROM `subtask` WHERE `id` = ?";
         
+        try {
+            $stmt = $db->prepare($getVal);
+            $stmt->execute([$_POST['id']]);
+            $prev = $stmt->fetch()['linked'];
+        }
+        catch (PDOException $e) {
+            consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
+            die('There was an error removing data from the database');
+        }
+
         try {
             $stmt = $db->prepare($rem);
             $stmt->execute([$_POST['id']]);
@@ -193,35 +240,39 @@ switch ($type) {
             consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
             die('There was an error removing data from the database');
         }
+
+        updateTaskCompletion($prev);
+
         break;
     case "subtaskCompletion":
-        $getVal = "SELECT `completed` FROM `subtask` WHERE id = ?";
+        $getVal = "SELECT `completed`, `linked` FROM `subtask` WHERE id = ?";
         $upd = "UPDATE `subtask` SET `completed` = ? WHERE id = ?";
         
         try {
             $stmt = $db->prepare($getVal);
             $stmt->execute([$_POST['id']]);
-            $prev = $stmt->fetch()['completed'];
+            $prev = $stmt->fetch();
         }
         catch (PDOException $e) {
             consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
             die('There was an error removing data from the database');
         }
 
-        $newval = ($prev == 1) ? 0 : 1;
+        $newval = ($prev['completed'] == 1) ? 0 : 1;
+        print($prev['completed']);
+        print($newval);
         try {
             $stmt = $db->prepare($upd);
             $stmt->execute([$newval, $_POST['id']]);
         }
         catch (PDOException $e) {
             consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
-            die('There was an error removing data from the database');
+            die('There was an error updating data from the database');
         }
 
-        // Update the completion status for the parent task
-        $getTasks = "SELECT `completed`";
+        updateTaskCompletion($prev['linked']);
 
         break;
 }
 
-header('Location: ' . $_SERVER['HTTP_REFERER']); ?>
+//header('Location: ' . $_SERVER['HTTP_REFERER']); ?>
