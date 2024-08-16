ca4e39ffde330e8ebad750b5046d5ff020962a74
waimea-jrgerraty <169000524+waimea-jrgerraty@users.noreply.github.com>
2024-08-16 16:24:02 +1200

    datetime normalisation
    subtask deadlines are converted to UTC and will be converted back into locale time when displayed

--- website/ServerFunctions.php
+++ website/ServerFunctions.php
@@ -1,11 +1,14 @@
 <?php
 require_once 'components/utils.php';
 $db = connectToDB();
+// Type defines what operation we are doing, and means I can have a single script do most of the functionality.
 $type = (isset($_POST['type'])) ? $_POST['type'] : $_REQUEST['type'];
 
+// Updates the completion value of a task based on the percentage of it's subtasks that are completed.
 function updateTaskCompletion($id) {
+    // Get the subtaks linked to this task
     $get = "SELECT `completed` FROM `subtask` WHERE `linked` = ?";
-    $db = connectToDB();
+    $db = connectToDB(); // function has a different scope so we need to connect to the DB again
 
     try {
         $stmt = $db->prepare($get);
@@ -17,6 +20,7 @@ function updateTaskCompletion($id) {
         die('There was an error updating task completion');
     }
 
+    // Calculate the fraction of completed subtasks
     $total = 0;
     $count = 0;
 
@@ -25,13 +29,16 @@ function updateTaskCompletion($id) {
         $total += $subtask['completed'];
     }
 
-    $fraction = $total / max($count, 1) * 100;
+    // Multiply by 100 to get a percentage
+    $percentage = $total / max($count, 1) * 100;
 
     $upd = "UPDATE `tasks` SET `completion` = ? WHERE `id` = ?";
 
+    // Update the completion value to the percentage
+    // This will be rounded to 0 dp when displayed
     try {
         $stmt = $db->prepare($upd);
-        $stmt->execute([$fraction, $id]);
+        $stmt->execute([$percentage, $id]);
     }
     catch (PDOException $e) {
         consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
@@ -63,7 +70,7 @@ switch ($type) {
             die('There was an error adding a new supercategory');
         }
         break;
-    case "sCategoryReorder":
+    case "sCategoryReorder": // Reorder a supercategory when dragged and dropped in the list
         $upd = "UPDATE `supercategory` SET `order` = ? WHERE `id` = ?";
 
         try {
@@ -76,7 +83,7 @@ switch ($type) {
         }
 
         break;
-    case "sCategoryRemove":
+    case "sCategoryRemove": // Remove a supercategory from the database
         $rem = "DELETE FROM `supercategory` WHERE `id` = ?"; // All descendant tables should be ON DELETE CASCADE
 
         try {
@@ -127,7 +134,7 @@ switch ($type) {
             die('There was an error removing data from the database');
         }
         break;
-    case "categoryReorder":
+    case "categoryReorder": // Reorder a category in the list when dragged and dropped
         $upd = "UPDATE `category` SET `order` = ? WHERE `id` = ?";
 
         try {
@@ -178,9 +185,10 @@ switch ($type) {
             die('There was an error removing data from the database');
         }
         break;
-    case "taskReorder":
+    case "taskReorder": // change the order of this task when dragged and dropped in the list
         $upd = "UPDATE `tasks` SET `order` = ?, `category` = ? WHERE `id` = ?";
 
+        // We may have to change category too, so also update the category foreign key
         try {
             $stmt = $db->prepare($upd);
             $stmt->execute([$_POST['order'],$_POST['cat'],$_POST['id']]);
@@ -191,10 +199,10 @@ switch ($type) {
         }
 
         break;
-    case "taskArchive":
-        $upd = "UPDATE `tasks` SET `category` = 2 WHERE `id` = ?"; // Archive has index 2
+    case "taskArchive": // Move a task to the archive
+        $upd = "UPDATE `tasks` SET `category` = 2 WHERE `id` = ?"; // Archive category has index 2
 
-        print_r($_POST);
+        // Move the task into the archive category (which is in the archive supercategory)
         try {
             $stmt = $db->prepare($upd);
             $stmt->execute([$_POST['id']]);
@@ -208,8 +216,9 @@ switch ($type) {
     case "subtask": // Add new subtask
         $imageData = null;
         $imageType = null;
-        // max 16MB
+        // max 16MB (there is a php file transfer limit as well I think so oh well)
         
+        // If the user provided an image, convert it to binary data to save into the database
         if (array_key_exists("image", $_FILES) && !$_FILES['image']['error']) {
             if ($_FILES["image"]["size"] <= 16777215) { 
                 [
@@ -221,8 +230,35 @@ switch ($type) {
 
         $ins = "INSERT INTO `subtask` (`task`, `image_type`, `image_data`, `deadline`, `linked`, `completed`) VALUES (?,?,?,?,?,?)";
         
+        // Get the users local timezone, so we can subtract the offset from their date to get it into UTC
+        $timezoneOffset = $_POST["timezone"];
+        // If the javascript that sets the timezone doesn't work then other things will probably break too.
+        // Therefore we tell the user to enable javascript or upgrade their browser if for some reason it doesn't support something
+        if ($timezoneOffset === null) {
+            die("Timezone not set, please enable javascript on this site, or upgrade your browser");
+        }
+        $timezoneOffset = intval($timezoneOffset); // will be negative if ahead of UTC, and positive if behind UTC.
         $deadline = ($_POST["deadline"] === '') ? null : $_POST["deadline"];
+        
+        // Deadlines are optional, so only do this if the user submits a deadline
+        if ($deadline !== null) {
+            $deadline = new DateTime($deadline, new DateTimeZone('UTC'));
+            
+            // Adjust the datetime based on the timezone offset
+            $timezoneOffsetInverval = new DateInterval('PT' . abs($timezoneOffset) . 'M');
+            
+            // Check if the offset is positive or negative and apply it
+            if ($timezoneOffset < 0) {
+                $deadline->sub($timezoneOffsetInverval); // Subtract if positive (behind UTC)
+            } else {
+                $deadline->add($timezoneOffsetInverval); // Add if negative (ahead of UTC)
+            }
 
+            // The datetime is now in UTC, format it for SQL insertion
+            $deadline = $deadline->format('Y-m-d H:i:s');
+        }
+
+        // Save the subtask to the database
         try {
             $stmt = $db->prepare($ins);
             $stmt->execute([$_POST['task'], $imageType, $imageData, $deadline, $_POST["linked"], 0]);
@@ -231,11 +267,13 @@ switch ($type) {
             consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
             die('There was an error adding a new subtask');
         }
+
         break;
-    case "subtaskDelete":
+    case "subtaskDelete": // Delete a subtask from the database
         $getVal = "SELECT `linked` FROM `subtask` WHERE id = ?";
         $rem = "DELETE FROM `subtask` WHERE `id` = ?";
         
+        // Get the task the subtask was linked to so we can update that tasks completion
         try {
             $stmt = $db->prepare($getVal);
             $stmt->execute([$_POST['id']]);
@@ -246,6 +284,7 @@ switch ($type) {
             die('There was an error removing data from the database');
         }
 
+        // Actually remove that task
         try {
             $stmt = $db->prepare($rem);
             $stmt->execute([$_POST['id']]);
@@ -255,26 +294,29 @@ switch ($type) {
             die('There was an error removing data from the database');
         }
 
+        // Update the completion status of the linked task
         updateTaskCompletion($prev);
 
         break;
     case "subtaskCompletion":
-        $getVal = "SELECT `completed`, `linked` FROM `subtask` WHERE id = ?";
+        $getVal = "SELECT * FROM `subtask` WHERE id = ?";
         $upd = "UPDATE `subtask` SET `completed` = ? WHERE id = ?";
         
+        // Get the current value and linked task
         try {
             $stmt = $db->prepare($getVal);
             $stmt->execute([$_POST['id']]);
-            $prev = $stmt->fetch();
+            $curr = $stmt->fetch();
         }
         catch (PDOException $e) {
             consoleLog($e->getMessage(), 'DB List Fetch', ERROR);
             die('There was an error removing data from the database');
         }
 
-        $newval = ($prev['completed'] == 1) ? 0 : 1;
-        print($prev['completed']);
-        print($newval);
+        // Invert the current value
+        $newval = ($curr['completed'] == 1) ? 0 : 1;
+
+        // Replace the saved value with the inverted value
         try {
             $stmt = $db->prepare($upd);
             $stmt->execute([$newval, $_POST['id']]);
@@ -284,7 +326,8 @@ switch ($type) {
             die('There was an error updating data from the database');
         }
 
-        updateTaskCompletion($prev['linked']);
+        // Update the completion status of the linked task
+        updateTaskCompletion($curr['linked']);
 
         break;
 }
